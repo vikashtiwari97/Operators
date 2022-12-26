37ebc78d0dfdf929d27d5293de8d1a2529418f2f
Vikash Tiwari <95138490+vikashtiwari97@users.noreply.github.com>
2022-12-26 12:02:37 +0530

    Add files via upload

---
+++ equal.cpp
@@ -0,0 +1,13 @@
+//Write a program to check whether two numbers (entered by user) are equal or not.
+#include<iostream>
+
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the value of a"<<endl;
+    cin>>a;
+    cout<<"Enter the value of b"<<endl;
+    cin>>b;
+    cout<<(a==b)?1:0;
+    return 0;
+}
